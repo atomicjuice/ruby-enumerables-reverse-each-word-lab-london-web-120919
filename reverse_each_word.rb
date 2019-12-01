285fb09e14c108203aa935e9c6b00065a434fb10
keemo <keemowatson1@gmail.com>
2019-12-01 17:00:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  words_array = string.split(" ")
+  array = string.split(" ")
   new_array = []
-  new_array = words_array.collect {|word| word.reverse }
+  new_array = array.collect {|word| word.reverse }
   new_array.join(" ")
 end
